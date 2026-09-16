889c820bbffd8da3ac527ccea00c5bc95ccc6935
juanesgl <juan.sgarcia@mail.escuelaing.edu.co>
2026-09-16 07:45:29 -0500

    feat: añado compose y init.sql para que funcione con Docker

---
+++ init.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS blueprints (
+    author VARCHAR(100) NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    PRIMARY KEY (author, name)
+);
+
+CREATE TABLE IF NOT EXISTS points (
+    id SERIAL PRIMARY KEY
+    blueprint_author VARCHAR(100) NOT NULL,
+    blueprint_name VARCHAR(100) NOT NULL,
+    x INT NOT NULL,
+    y INT NOT NULL,
+    point_order INT NOT NULL,
+    FOREIGN KEY (blueprint_author, blueprint_name) REFERENCES blueprints(author, name) ON DELETE CASCADE
+);
